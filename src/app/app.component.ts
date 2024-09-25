fe8688517b901e36aee850f531a2ef0d2e5b7b2b
Ricardo Huaripata <102578959+ricardohuaripata@users.noreply.github.com>
2024-09-25 03:22:00 +0200

    newsletter section implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { AnnouncementBarComponent } from "./components/announcement-bar/announcement-bar.component";
+import { NewsletterComponent } from "./components/newsletter/newsletter.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, AnnouncementBarComponent],
+  imports: [RouterOutlet, NavbarComponent, AnnouncementBarComponent, NewsletterComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
