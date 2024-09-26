d3f3718bbc9a4ca24057e2b4c710133ec67294d4
Ricardo Huaripata <102578959+ricardohuaripata@users.noreply.github.com>
2024-09-26 02:21:30 +0200

    footer implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { AnnouncementBarComponent } from "./components/announcement-bar/announcement-bar.component";
 import { NewsletterComponent } from "./components/newsletter/newsletter.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, AnnouncementBarComponent, NewsletterComponent],
+  imports: [RouterOutlet, NavbarComponent, AnnouncementBarComponent, NewsletterComponent, FooterComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
