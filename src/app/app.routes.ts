7317eb860a77bf7f882af0b5f4ae6d0990a0d18b
Ricardo Huaripata <102578959+ricardohuaripata@users.noreply.github.com>
2024-10-01 04:25:45 +0200

    page not found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
   {
@@ -8,7 +9,7 @@ export const routes: Routes = [
   },
   {
     path: '**',
-    redirectTo: '',
-    pathMatch: 'full',
+    component: NotFoundComponent,
+    title: 'Page Not Found',
   },
 ];
